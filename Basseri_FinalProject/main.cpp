4c0d4b8bceecfab1f5d9013bd1c15dd38805258d
Benjamin Ickies <benjamin@ickies.com>
2018-08-08 17:47:44 -0400

    Initial Commit

---
+++ Basseri_FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Basseri_FinalProject
+//
+//  Created by Benjamin Basseri on 8/8/18.
+//  Copyright © 2018 Benjamin Basseri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
